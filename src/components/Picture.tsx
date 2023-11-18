7687c88f5935a352da5b78dd1a2c08dc5a4937ff
William Kong <wilko@Williams-MacBook-Pro.local>
2023-11-18 17:16:16 -0500

    fixed picture-size

---
+++ src/components/Picture.tsx
@@ -0,0 +1,14 @@
+
+interface PictureProps {
+    src: string;
+}
+  
+const Picture: React.FC<PictureProps> = ({ src }) => {
+    return (
+        <div className="m-auto w-64 h-64 border border-gray-300 overflow-hidden">
+        <img src={src} className="w-full h-full object-cover" />
+        </div>
+    );
+};
+  
+export default Picture;
