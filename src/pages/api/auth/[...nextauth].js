19a0eba7412cd1a9d37d3d6f4d17e72c0a0220d4
arael <jonasg@umich.edu>
2023-11-18 17:21:31 -0500

    prisma working

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const prisma = new PrismaClient()
+
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
