c02853ffde42c5f4d27139d1344fed2201e64249
arael <jonasg@umich.edu>
2023-11-18 15:40:11 -0500

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 function Header() {
     return (
-        <div className="flex flex-row justify-between bg-emerald-400">
-            <div className="ml-1">
+        <div className="flex justify-between bg-gray-800 h-10 align-center text-milk text-slate-100">
+            <div className="flex items-center ml-1">
                 <Link className="pl-5" href="/">Home</Link>
             </div>
-            <div className="mr-1">
+            <div className="flex items-center mr-1">
                 <Link className="pr-5" href="/play">Play</Link>
                 <Link className="pr-5" href="/evolution">Evolution</Link>
             </div>
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -3,6 +3,9 @@ import Header from "../components/header";
 
 // TODO
 // Maybe remove images after they've been seen
+// Timers for feedback / showing correct
+// Stretch images to card
+// TODO if an answer is shown, you don't get a point and your streak is lost
 
 const IMAGES = [
     "Goodbye.png",
@@ -41,43 +44,48 @@ export default function Play() {
     function generateNew() {
         setImage(generateRandom);
         setFeedback(null);
+        setCorrect(false);
     }
 
     function submitGuess() {
         if (generateImageCorrect(image || "") === generateGuessCorrect(text)) {
             setFeedback("correct! +1");
+            setImage(generateRandom);
+            setText("");
+            setCorrect(false);
         } else {
-            setFeedback("Wrong");
+            setFeedback("wrong, try again!");
         }
     }
 
     return (
         <main>
         <Header />
-        <div className="flex flex-col w-1/2 m-auto">
-            <h1>Play</h1>
+        <div className="flex flex-col w-1/2 m-auto text-center">
+            <div className="h-60">
             { image &&
                 <>
-                    <img width="300px" height="300px" src={image} />
+                    <img className="m-auto" width="210px" height="210px" src={image} />
                     { correct ?
-                        <p className="h-2">Correct: { generateImageCorrect(image) }</p>
+                        <p className="h-2 mb-3">answer: { generateImageCorrect(image) }</p>
                         :
-                        <p className="h-2" />
+                        <p className="h-2 mb-3" />
                     }
                 </>
             }
-        
-            <button onClick={ () => setCorrect(!correct) }>Show correct</button>
-            <input type="text" placeholder="enter the word here..." onChange={ (event) => setText(event.target.value) } />
-            <button onClick={submitGuess}>Guess!</button>
+            </div>
+            <button className="text-red-400 mt-5" onClick={ () => setCorrect(!correct) }>Show Answer</button>
+            <input className="ml-auto mr-auto w-60 mt-5 mb-5 p-1 outline-dashed rounded" type="text" placeholder="enter the word here..." value={text} onChange={ (event) => setText(event.target.value) } />
+            <button className="m-auto mb-4 bg-blue-700 text-white border border-blue-700 font-bold py-2 px-6 rounded-lg w-40" onClick={submitGuess}>Guess!</button>
             { feedback ?
-                <p className="h-2">{ feedback }</p>
+                <p className="h-2 mb-5">{ feedback }</p>
                 :
-                <p className="h-2" />
+                <p className="h-2 mb-5" />
             }
-            <button onClick={generateNew}>Generate new</button>
+            <button className="m-auto bg-gray-200 hover:bg-gray-300 border border-gray-400 text-black font-bold py-2 px-6 rounded-md w-40" onClick={generateNew}>
+                Generate New
+            </button>
         </div>
         </main>
     )
 }
-
