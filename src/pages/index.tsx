a6d7709d4d2ae7840d2605e3d1e6aa8e769c23c1
arael <jonasg@umich.edu>
2023-11-18 15:50:11 -0500

    home page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,11 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <h1>Yo</h1>
-      <Link href="/play" >PLAY</Link>
+      <div className="flex flex-col text-center">
+        <h1 className="text-4xl mt-8">Learn ASL and evolve characters!</h1>
+        <Link className="m-auto mt-8 bg-gray-200 hover:bg-gray-300 border border-gray-400 text-black font-bold py-2 px-6 rounded-md w-60" href="/profile">View Profile or Sign Up</Link>
+        <Link className="text-xl m-auto mt-8 bg-blue-700 text-white border border-blue-700 font-bold py-2 px-6 rounded-lg w-40" href="/play">Play Now!</Link>
+      </div>
     </main>
   )
 }
