eb26d7f7adf993a4f093de3a1b5ebd423d12987e
arael <jonasg@umich.edu>
2023-11-18 21:10:40 -0500

    profile styling

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-const prisma = new PrismaClient()
+import prisma from "@/server/db";
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
