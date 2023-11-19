4d52f6c677b76f19022cc70fa82ce0ea6583c746
William Kong <wilko@Williams-MacBook-Pro.local>
2023-11-18 23:57:45 -0500

    logo fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
+import Logo from "./Logo";
 
 function Header() {
     return (
-        <div className="flex justify-between bg-gray-800 h-16 align-center text-milk text-slate-100">
-            <Link className="ml-1 font-bold pl-10" href="/">
-                <img src="ASL_EVO_LOGO.png" />
-            </Link>
+        <nav className="flex justify-between bg-gray-800 h-16 align-center text-milk text-slate-100">
+            <Logo />
             <div className="flex items-center mr-1">
                 <Link className="font-black pr-14 text-3xl" href="/play">Play!</Link>
                 <Link className="font-black pr-14 text-3xl" href="/profile">Profile</Link>
             </div>
-        </div>
+        </nav>
     );
 }
 
