54b69ccf1808838520f353d35985ba93a0a69b2a
William Kong <wilko@Williams-MacBook-Pro.local>
2023-11-18 22:03:35 -0500

    changed button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 function Header() {
     return (
         <div className="flex justify-between bg-gray-800 h-16 align-center text-milk text-slate-100">
-            <div className="flex items-center ml-1 text-xl">
-                <Link className="font-bold pl-10" href="/"><img width="375" height="375" src="ASL_EVO_LOGO.png" /></Link>
-            </div>
+            <Link className="ml-1 font-bold pl-10" href="/">
+                <img src="ASL_EVO_LOGO.png" />
+            </Link>
             <div className="flex items-center mr-1">
                 <Link className="font-black pr-14 text-3xl" href="/play">Play!</Link>
                 <Link className="font-black pr-14 text-3xl" href="/profile">Profile</Link>
